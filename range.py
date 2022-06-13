ce3ab6086085db38a754bc073b67faba4d36fa10
thunderboltmchase <102049790+thunderboltmchase@users.noreply.github.com>
2022-06-13 19:57:15 -0400

    Add files via upload

---
+++ range.py
@@ -0,0 +1,14 @@
+def my_range(m, n):
+# End goal: {0,1,2,3,4}
+# While loop
+# count from m to n
+
+    output = []
+    while (m < n):
+        output.append(m)
+        m+= 1
+    
+        
+    return output
+print (my_range(0,5))
+    
